da7076615fe15f4215d9d7edab84547e6c517d49
Mahmud Hasan <mahmud.hasan2006@gmail.com>
2023-06-06 08:19:32 +0600

    vercel deploy done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -20,6 +21,22 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if (error) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +48,18 @@ async function run() {
         // create booking collection database
         const bookingCollection = client.db("carDoctor").collection('bookings')
 
+        // jwt 
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1h'
+            });
+            res.send({ token });
+        })
+
+        // services route
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -50,13 +79,21 @@ async function run() {
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
         })
+
         // to get only show booking order with email
-        app.get('/bookings', async (req, res) => {
-            console.log(req.query.email);
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            const decoded = req.decoded;
+            console.log('came back after verify', decoded);
+
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ error: 1, message: 'forbidden access' })
+            }
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
             }
+
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
         })
